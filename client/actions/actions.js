26a30be3ac7b16bdadb779faad4f3b993c44acdf
Andrew Tang <atreeleaf@gmail.com>
2019-08-07 18:49:57 -0700

    updated reducers

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -12,10 +12,26 @@ export const searchResults = results => ({
   payload: results,
 });
 
+export const setView = view => ({
+  type: types.SET_VIEW,
+  payload: view
+})
+
+export const updateZipCode = zipcode => ({
+  type: types.UPDATE_ZIP_CODE,
+  payload: zipcode,
+})
 
 // thunk that handles search request
-export const submitSearch = () => (dispatch, getState) => {
-  dispatch(searchResults([{company: "Pikachu", price: 42, hashtag: 'pikaPIKAAAA'}]))
+export const submitSearch = (zipcode) => (dispatch, getState) => {
+  // make a fetch request to yelp / Eventbrite
+  fetch('/api');
+ 
+  // promisify both requests 
 
+  // promise.all - and send the results back to the client. 
+
+
+  dispatch(searchResults(zipcode));
 };
 
